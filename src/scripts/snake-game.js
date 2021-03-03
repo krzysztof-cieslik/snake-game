a3c2860b0e4013f87b9fa7e2c6dadf990ad086db
krzysztof-cieslik <mrkrzysiex@gmail.com>
2021-03-03 13:33:48 +0100

    Krzysztof - refactor: adjust margins and snake speed, add flex to to some containers.

--- src/scripts/snake-game.js
+++ src/scripts/snake-game.js
@@ -2,15 +2,8 @@ let canvas = document.getElementById("snake-game");
 let ctx = canvas.getContext("2d");
 let ax = canvas.width / 2 - 10;
 let ay = canvas.height / 2 - 10;
-let bx = canvas.width / 2 - 10;
-let by = canvas.height / 2 - 10;
-let cx = canvas.width / 2 - 10;
-let cy = canvas.height / 2 - 10;
-let dx = canvas.width / 2 - 10;
-let dy = canvas.height / 2 - 10;
-let move = 20;
 
-for (let i = 20; i < canvas.width; i += 20) {
+/*for (let i = 20; i < canvas.width; i += 20) {
     ctx.beginPath();
     ctx.moveTo(i, 0);
     ctx.lineTo(i, canvas.height);
@@ -25,36 +18,37 @@ for (let i = 20; i < canvas.height; i += 20) {
     ctx.strokeStyle = "rgb(181, 181, 181)";
     ctx.stroke();
     ctx.closePath();
+}*/
+
+let color = 0;
+
+for (let i = 0; i < canvas.height; i += 20) {
+    for (let j = 0; j < canvas.width; j += 20) {
+        ctx.beginPath();
+        ctx.rect(j, i, 20, 20);
+        if (color === 0) {
+            ctx.fillStyle = "#99c2ff";
+            color = 1;
+        } else {
+            ctx.fillStyle = "white";
+            color = 0;
+        }
+        ctx.fill();
+        ctx.closePath();
+    }
 }
 
-
-function draw() {
+function drawSnake() {
     ctx.beginPath();
     ctx.rect(ax, ay, 20, 20);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
-    ax += move;
-    ay += move;
-    ctx.beginPath();
-    ctx.rect(bx, by, 20, 20);
-    ctx.fill();
-    ctx.closePath();
-    bx -= move;
-    by += move;
-    ctx.beginPath();
-    ctx.rect(cx, cy, 20, 20);
-    ctx.fill();
-    ctx.closePath();
-    cx += move;
-    cy -= move;
-    ctx.beginPath();
-    ctx.rect(dx, dy, 20, 20);
-    ctx.fillStyle = "blue";
-    ctx.fill();
-    ctx.closePath();
-    dx -= move;
-    dy -= move;
 }
 
-setInterval(draw, 1000);
+function draw() {
+    drawSnake();
+    ay -= 20;
+}
+
+setInterval(draw, 100);
